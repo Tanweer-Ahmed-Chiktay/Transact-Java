36c2ac4679babd76aacc1b2f1c549475a528d316
Tanweer Ahmed Chiktay <76688968+Tanweer-Ahmed-Chiktay@users.noreply.github.com>
2024-04-20 17:14:30 +0200

    Update user.java

---
+++ user.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class user {
+    String firstName;
+    String lastName;
+    int age;
+    String cardNo;
+    String pin;
+    Date exp;
+    String ccv;
+
+    public user(String firstName, String lastName, int age, String cardNo, String pin, Date exp, String ccv) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.cardNo = cardNo;
+        this.pin = pin;
+        this.exp = exp;
+        this.ccv = ccv;
+    }
+}
+
+
